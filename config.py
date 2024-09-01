68356b4b48a08bfea8cbd8ae0ac535b55e01166f
Nosna132 <141562783+Nosna132@users.noreply.github.com>
2024-09-01 18:24:27 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.czuu3lz9ygu2.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "tancheefung-employee"
+customregion = "us-east-1"
+
